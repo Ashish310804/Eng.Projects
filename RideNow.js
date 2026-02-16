b6a0846bac8443ccb0729b7e0f1fe54c61a5c7e6
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-02-17 01:13:53 +0530

    Project for a rental system

---
+++ RideNow.js
@@ -0,0 +1,25 @@
+function searchVehicle() {
+    let typeInput = document.getElementById("type").value.toLowerCase();
+    let cards = document.querySelectorAll(".card");
+    let resultText = document.getElementById("searchResult");
+
+    let found = false;
+
+    cards.forEach(card => {
+        let vehicleType = card.getAttribute("data-type");
+
+        if (vehicleType.includes(typeInput) || typeInput === "") {
+            card.style.display = "block";
+            found = true;
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    if (!found) {
+        resultText.innerText = "No vehicles found!";
+        resultText.style.color = "red";
+    } else {
+        resultText.innerText = "";
+    }
+}
