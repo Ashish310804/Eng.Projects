d49361e32beefd9917bad3d53e412878853e0217
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-10 23:30:05 +0530

    Update Quiz1.js

--- Project02/Quiz1.js
+++ Project02/Quiz1.js
@@ -107,6 +107,7 @@ function selectAnswer(event) {
 
   // optimization check
   if (answersDisabled) return;
+  
   answersDisabled = true;
   const selectedButton = event.target;
   const isCorrect = selectedButton.dataset.correct === "true";
